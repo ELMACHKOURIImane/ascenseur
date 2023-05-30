cb389972299b32887e3e6e91d564901c00d8ed8f
ELMACHKOURIImane <114507748+ELMACHKOURIImane@users.noreply.github.com>
2023-05-30 16:47:19 +0100

    first commit

---
+++ src/main/java/Ascenseur1/Asenceur.java
@@ -0,0 +1,112 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ascenseur1;
+
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JLabel;
+import javax.swing.JRadioButton;
+
+/**
+ *
+ * @author DELL
+ */
+public class Asenceur extends Thread {
+private int placeOcupes =0 ; 
+private ArrayList<Personne> personnesIn = new ArrayList<Personne>();
+private Asenceur Etages[] ; 
+int nbr_Etages ; 
+int idAscenseur ; 
+int positionActull ; 
+private JLabel lblEtage ;
+private JRadioButton btnMonter ;
+private JRadioButton btnDescender ;
+private JRadioButton btnArreter ; 
+ 
+private JRadioButton btnEtages[] ; 
+
+Personne p ; 
+    public Asenceur( int nbr_Etages ,int idAscenseur, JLabel lblEtage , JRadioButton Monter , JRadioButton Descender ,JRadioButton []btnEtages , JRadioButton btnArreter  ) {
+        this.Etages  = new Asenceur[nbr_Etages];
+        this.nbr_Etages = nbr_Etages;
+        this.positionActull =  1 ;
+        this.idAscenseur = idAscenseur ; 
+        this.lblEtage = lblEtage ; 
+        this.btnMonter = Monter ;
+        this.btnDescender =  Descender ; 
+        this.btnEtages = btnEtages ;
+        this.btnArreter = btnArreter ; 
+    }
+synchronized boolean acceptAppel(Personne personne ){
+    if(this.placeOcupes <4 ){
+        personnesIn.add(personne);
+        placeOcupes++ ; 
+        System.out.format("La distination est %s \n", personne.getEtage());
+        return true ; 
+    }
+    else {
+        System.out.println("L'ascenseur est plein ");
+        return false ; 
+    }
+}
+synchronized void leaveAscenseur(Personne p){
+    this.placeOcupes-- ; 
+    personnesIn.remove(p) ; 
+    System.out.format("L'ascenseur est dans l'etage %s ", p.getEtage()) ; 
+}
+synchronized void Mouve(Personne p ){
+    while(this.positionActull < p.getEtage() ){ 
+        System.out.println("L'ascenseur est entrain de monter ");
+        try {
+            Thread.sleep((long) (3000*Math.random()));
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Asenceur.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this.lblEtage.setLocation(6,p.goTo(positionActull));
+        this.btnEtages[positionActull-1].setSelected(true);
+        System.out.println(this); 
+       this.Etages[positionActull] = this.Etages[positionActull++] ; 
+       btnMonter.setSelected(true);
+        try {
+            Thread.sleep((long) (3000*Math.random()));
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Asenceur.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+        while(this.positionActull > p.getEtage()){
+           System.out.println("L'ascenseur est entrain de descendre ");
+        try {
+            Thread.sleep((long) (3000*Math.random()));
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Asenceur.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            this.lblEtage.setLocation(6,p.goTo(positionActull));
+           this.btnEtages[positionActull-1].setSelected(true);
+            System.out.println(this);
+           this.Etages[positionActull] = this.Etages[positionActull--] ;
+           btnDescender.setSelected(true);
+        try {
+            Thread.sleep((long) (3000*Math.random()));
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Asenceur.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+       if(this.positionActull == p.getEtage()){
+       System.out.println("L'ascenseur est arreter "); 
+       this.lblEtage.setLocation(6,p.goTo(positionActull));
+        this.btnEtages[positionActull-1].setSelected(true);
+        System.out.println(this); 
+       this.btnArreter.setSelected(true); 
+       this.leaveAscenseur(p);
+        } 
+    
+}
+   @Override
+    public String toString() {
+        return " L'ascenseur numero "+this.idAscenseur+" est dans l'etage "+this.positionActull ; 
+    }
+}
---
+++ src/main/java/Ascenseur2/Personne.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ascenseur2;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author DELL
+ */
+public class Personne implements Runnable  {
+    
+    private int Etage ; 
+    private int numeroP ; 
+    private Asenceur ascenseur ;
+
+    public Personne(int Etage, Asenceur As , int numeroP) {
+        this.Etage = Etage;
+        this.ascenseur = As ; 
+        this.numeroP = numeroP ;   
+    }
+public void appel(){
+System.out.println("La personne "+numeroP+" appele l'ascenseur");
+}
+    public int getEtage() {
+        return Etage;
+    }
+
+    @Override
+    public void run() {
+         this.appel();
+    try { 
+        Thread.sleep((long) (5000*Math.random()));
+    } catch (InterruptedException ex) {
+        Logger.getLogger(Asenceur.class.getName()).log(Level.SEVERE, null, ex);
+    }
+    this.ascenseur.acceptAppel(this);
+    this.ascenseur.Mouve(this);
+    }
+    public int goTo(int etage)
+	{
+		int yEtage = 0;
+		switch(etage)
+		{
+			case 1 :
+				yEtage = 334;
+				break;
+			case 2 :
+				yEtage = 252;
+				break;
+			case 3 :
+				yEtage = 170;
+				break;
+			case 4 :
+				yEtage = 88;
+				break;
+			case 5 :
+				yEtage = 6;
+				break;
+		}
+		return yEtage;
+	}
+public static void main(String[] agrs){
+//       Asenceur As = new Asenceur(5 , 1 );
+//    Personne p = new Personne(3 , As , 2) ;
+//    Personne p1 = new Personne(2 , As, 1) ;
+//    Thread Th = new Thread(p) ;
+//    Thread Th1 = new Thread(p1) ;
+//    Th.start();
+//    Th1.start();
+     
+}  
+
+    
+    
+}
